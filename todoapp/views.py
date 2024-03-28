abcb2881cd644972cdc31790407eb9d9ffccb011
leo88 <nishinole88@gmail.com>
2024-03-28 17:56:55 +0900

    templatesの作成

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Task
 
 
+class TaskList(ListView):
+    model = Task
+    context_object_name = "tasks"
 
-def taskList(request):
-    return HttpResponse("<h1>taskList</h1>")
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+    
+
