4fb495b15361ae9f4cac3226a826f14543362748
leo88 <nishinole88@gmail.com>
2024-03-28 18:14:49 +0900

    タスク追加のフォームの作成

--- todoapp/views.py
+++ todoapp/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
 from .models import Task
 
 
@@ -13,4 +16,7 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = "task"
     
-
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("taskList")
